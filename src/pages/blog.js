1cfe70418ae8df2e13ddcd8fca9232686a7f8ea6
jmstickney-cloudinary <jonathan.stickney@cloudinary.com>
2021-05-11 09:47:33 -0400

    removed white space

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -50,7 +50,6 @@ export const pageQuery = graphql`
               ...GatsbyContentfulFluid_tracedSVG
             }
           }
-
           description {
             childMarkdownRemark {
               html
