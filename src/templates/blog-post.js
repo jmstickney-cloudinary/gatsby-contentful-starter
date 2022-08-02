0fd0eae23528840e9d7e0e7821c3bac1cd66a4ee
jmstickney-cloudinary <jonathan.stickney@cloudinary.com>
2022-08-02 10:31:39 -0400

    trying to fix width

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -47,7 +47,7 @@ class BlogPostTemplate extends React.Component {
 
 function renderCloudinaryImages(post) {
   if (post.cloudinaryImage) {
-    console.log(cloudinaryImage.secure_url);
+    console.log(cloudinaryImage);
     return (
       <div>
         {post.cloudinaryImage.map((i) => (
